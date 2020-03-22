c50c580b3dfb570154a01e5b448265ec0a0dcbf8
javiert01 <javotinoco@hotmail.com>
2020-03-22 17:40:38 -0500

    small fixes

--- src/app/components/commerce-registration/commerce-registration.component.ts
+++ src/app/components/commerce-registration/commerce-registration.component.ts
@@ -1,48 +1,48 @@
-import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { CommerceService } from 'src/app/services/commerce.service';
 
 @Component({
-  selector: "app-commerce-registration",
-  templateUrl: "./commerce-registration.component.html",
-  styleUrls: ["./commerce-registration.component.css"]
+  selector: 'app-commerce-registration',
+  templateUrl: './commerce-registration.component.html',
+  styleUrls: ['./commerce-registration.component.css']
 })
 export class CommerceRegistrationComponent implements OnInit {
   commerceCategories = [
-    "Abarrotes",
-    "Tienda",
-    "Restaurante",
-    "Farmacia",
-    "Víveres/frutería",
-    "Micromercado",
-    "Otros"
+    'Abarrotes',
+    'Tienda',
+    'Restaurante',
+    'Farmacia',
+    'Víveres/frutería',
+    'Micromercado',
+    'Otros'
   ];
 
   provinces = [
-    "Azuay",
-    "Bolívar",
-    "Cañar",
-    "Carchi",
-    "Chimborazo",
-    "Cotopaxi",
-    "El Oro",
-    "Esmeraldas",
-    "Galápagos",
-    "Guayas",
-    "Imbabura",
-    "Loja",
-    "Los Ríos",
-    "Manabí",
-    "Morona Santiago",
-    "Napo",
-    "Orellana",
-    "Pastaza",
-    "Pichincha",
-    "Santa Elena",
-    "Santo Domingo de los Tsáchilas",
-    "Sucumbíos",
-    "Tungurahua",
-    "Zamora Chinchipe"
+    'Azuay',
+    'Bolívar',
+    'Cañar',
+    'Carchi',
+    'Chimborazo',
+    'Cotopaxi',
+    'El Oro',
+    'Esmeraldas',
+    'Galápagos',
+    'Guayas',
+    'Imbabura',
+    'Loja',
+    'Los Ríos',
+    'Manabí',
+    'Morona Santiago',
+    'Napo',
+    'Orellana',
+    'Pastaza',
+    'Pichincha',
+    'Santa Elena',
+    'Santo Domingo de los Tsáchilas',
+    'Sucumbíos',
+    'Tungurahua',
+    'Zamora Chinchipe'
   ];
 
   registerForm: FormGroup;
@@ -56,7 +56,7 @@ export class CommerceRegistrationComponent implements OnInit {
       ownerLastName: new FormControl(null, Validators.required),
       phone: new FormControl(null, Validators.required),
       commerceName: new FormControl(null, Validators.required),
-      categoryCommerce: new FormControl(null, Validators.required),
+      commerceCategory: new FormControl(null, Validators.required),
       hourOpen: new FormControl(null, Validators.required),
       hourClose: new FormControl(null, Validators.required),
       province: new FormControl(null, Validators.required),
@@ -75,21 +75,22 @@ export class CommerceRegistrationComponent implements OnInit {
       ownerLastName: this.registerForm.get('ownerLastName').value,
       phone: this.registerForm.get('phone').value,
       commerceName: this.registerForm.get('commerceName').value,
-      categoryCommerce: this.registerForm.get('categoryCommerce').value,
-      hourOpen: this.registerForm.get('hourOpen').value,
-      hourClose: this.registerForm.get('hourClose').value,
-      province:this.registerForm.get('province').value,
+      commerceCategory: this.registerForm.get('commerceCategory').value,
+      hourOpen: '09:00', // this.registerForm.get('hourOpen').value,
+      hourClose: '23:00', // this.registerForm.get('hourClose').value,
+      province: this.registerForm.get('province').value,
       city: this.registerForm.get('city').value,
-      neighborhood: this.registerForm.get('neighborhood').value,
+      neighborhood: 'Floresta', // this.registerForm.get('neighborhood').value,
       address: this.registerForm.get('address').value,
-      location: this.registerForm.get('location').value,
+      location:  {'type': 'Point', 'coordinates': [-78.4877785,-0.2043812]}, // this.registerForm.get('location').value,
       reference: this.registerForm.get('reference').value,
       commerceDescription: this.registerForm.get('commerceDescription').value
     };
+    console.log(this.commerce);
     this.commerceService.createNewCommerce(this.commerce)
     .subscribe((data) => {
       console.log('registro exitoso', data);
-    },(err)=>{
+    }, (err) => {
       console.error(err);
     });
 
