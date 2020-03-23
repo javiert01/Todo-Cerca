9292f7761adfb60d3167cbacaee1dfcff2b11008
javiert01 <javotinoco@hotmail.com>
2020-03-22 20:58:51 -0500

    update angular v8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 import { ReactiveFormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
+import { AgmCoreModule } from "@agm/core";
+
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { CommerceRegistrationComponent } from "./components/commerce-registration/commerce-registration.component";
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,16 @@ import { HttpClientModule } from '@angular/common/http';
     FooterComponent,
     CommerceRegistrationComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AgmCoreModule.forRoot({
+      apiKey: "AIzaSyAswlswB4nSDMpRuWl9MLTJVBLc4x9J7XE",
+      libraries: ["places"]
+    })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
