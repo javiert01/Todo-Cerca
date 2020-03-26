c9dce3289b4c4a0689890c6f8e922c683065913f
Armando <Tapia>
2020-03-26 17:36:32 -0500

    Add link to polices and scroll

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,21 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router, NavigationStart } from "@angular/router";
+import { tap } from "rxjs/operators";
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.css"]
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  showTerms = false;
+  constructor(private _router: Router) {
+    this._router.events
+      .pipe(
+        tap(e => {
+          if (e instanceof NavigationStart) {
+            if (e.url === "/gracias") {
+              this.showTerms = true;
+            } else {
+              this.showTerms = false;
+            }
+          }
+        })
+      )
+      .subscribe();
   }
+
+  ngOnInit() {}
 
   onGoToTop() {
     document
-    .querySelector('#section-3-registro')
-    .scrollIntoView({ behavior: 'smooth' });
+      .querySelector("#section-3-registro")
+      .scrollIntoView({ behavior: "smooth" });
   }
-
 }
