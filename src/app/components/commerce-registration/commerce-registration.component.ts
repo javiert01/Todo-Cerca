c97de7a80752e3b955ddb984e730188b8b82d1bd
javiert01 <javotinoco@hotmail.com>
2020-03-27 10:17:23 -0500

    fixes in timepicker

--- src/app/components/commerce-registration/commerce-registration.component.ts
+++ src/app/components/commerce-registration/commerce-registration.component.ts
@@ -320,11 +320,29 @@ export class CommerceRegistrationComponent implements OnInit {
   }
 
   minutesOfDay(m) {
+    m = this.convertTime12to24(m);
     const hourMinutesClose = m.split(':');
     const hours = Number(hourMinutesClose[0]);
     const minutes = Number(hourMinutesClose[1]);
     return minutes + hours * 60;
   }
+
+  convertTime12to24(time12h) {
+   const [time, modifier] = time12h.split(' ');
+
+    let [hours, minutes] = time.split(':');
+
+    if (hours === '12') {
+      hours = '00';
+    }
+
+    if (modifier === 'PM') {
+      hours = parseInt(hours, 10) + 12;
+    }
+
+    return `${hours}:${minutes}`;
+  }
+
 
   onSetCityMap(city) {
     console.log('changing city', city);
