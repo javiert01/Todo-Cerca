074b474b47166b105ba74b447da682740c425c13
Armando <Tapia>
2020-03-31 17:40:28 -0500

    add one method to commerce service

--- src/app/components/admin/download-excel-dialog/download-excel-dialog.component.ts
+++ src/app/components/admin/download-excel-dialog/download-excel-dialog.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Inject } from "@angular/core";
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { FileService } from "src/app/services/file.service";
+import { CommerceService } from "src/app/services/commerce.service";
+import { tap } from "rxjs/operators";
 
 @Component({
   selector: "app-download-excel-dialog",
@@ -7,7 +10,18 @@ import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
   styleUrls: ["./download-excel-dialog.component.css"]
 })
 export class DownloadExcelDialogComponent implements OnInit {
+  allowed;
+  pageNumber;
+  category;
+
+  commercesBeforePrint = [];
+  //=============================================================
+  dataCommerces: any = [];
+  //=============================================================
+
   constructor(
+    public fileService: FileService,
+    public commerceService: CommerceService,
     private dialogRef: MatDialogRef<DownloadExcelDialogComponent>,
     @Inject(MAT_DIALOG_DATA) data
   ) {}
@@ -15,5 +29,48 @@ export class DownloadExcelDialogComponent implements OnInit {
   ngOnInit(): void {}
   close() {
     this.dialogRef.close();
+  }
+
+  downloadAll() {
+    this.commerceService
+      .getAllCommercesNoPagination()
+      .pipe(
+        tap(content => {
+          console.log("Contenido: ", content);
+          this.commercesBeforePrint = content["commerces"];
+          this.constructCommercePrint();
+          this.exportAsXLSX();
+        })
+      )
+      .subscribe();
+  }
+  downloadPage() {
+    this.allowed = false;
+    this.pageNumber = 1;
+    this.category = "all";
+    this.commerceService
+      .getAllCommerces(this.allowed, this.pageNumber, this.category)
+      .pipe(
+        tap(content => {
+          // console.log("Contenido: ", content);
+          this.commercesBeforePrint = content["commercesPaginated"];
+          this.constructCommercePrint();
+          this.exportAsXLSX();
+        })
+      )
+      .subscribe();
+  }
+  downloadSelected() {}
+
+  constructCommercePrint() {
+    this.commercesBeforePrint.forEach(comm => {
+      // console.log("Commercio: ", comm);
+      comm.category = comm["category"]["commerceCategory"];
+      console.log("Commercio: ", comm);
+      this.dataCommerces.push(comm);
+    });
+  }
+  exportAsXLSX(): void {
+    this.fileService.exportAsExcelFile(this.dataCommerces, "sample");
   }
 }
