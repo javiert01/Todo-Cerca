f7bbb6ff7bcf13060e6a351bcabf216b334c8674
javiert01 <javotinoco@hotmail.com>
2020-04-03 14:48:03 -0500

    var

--- src/app/shared/var.constants.ts
+++ src/app/shared/var.constants.ts
@@ -1,8 +1,9 @@
 // BACKEND LOCAL
-export const HOST = "http://localhost:1337";
+//export const HOST = "http://localhost:1337";
 
 // BACKEND OPTIMUS DESARROLLO TEST
 /*
 export const HOST = "https://todo-mas-cerca.herokuapp.com";
 */
-// export const HOST = "https://todo-mas-cerca-1.herokuapp.com";
+//
+export const HOST = "https://todo-mas-cerca-1.herokuapp.com";
