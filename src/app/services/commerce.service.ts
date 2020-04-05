47a53f2d469dc5ed63f17b724f7208c1b7f4f478
javiert01 <javotinoco@hotmail.com>
2020-04-05 10:51:31 -0500

    added minor styling

--- src/app/services/commerce.service.ts
+++ src/app/services/commerce.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { HOST } from "../shared/var.constants";
-import { Commerce } from "../models/commerce.model";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HOST } from '../shared/var.constants';
+import { Commerce } from '../models/commerce.model';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class CommerceService {
   url = `${HOST}/commerces`;
@@ -16,8 +16,8 @@ export class CommerceService {
   constructor(private http: HttpClient) {
     this.httpOptions = {
       headers: new HttpHeaders({
-        "Content-Type": "application/json",
-        Authorization: localStorage.getItem("token"),
+        'Content-Type': 'application/json',
+        Authorization: localStorage.getItem('token'),
       }),
     };
   }
@@ -64,6 +64,10 @@ export class CommerceService {
       `${this.url}/all/nopagination?allowed=${allow}`,
       this.httpOptions
     );
+  }
+
+  searchCommerce(param) {
+    return this.http.get<any>(`${this.url}/patterMatch?param=${param}`);
   }
 
   getCommerce() {
