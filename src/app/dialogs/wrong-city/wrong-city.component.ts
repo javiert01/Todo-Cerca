b9ca9491acf5402e17cf626031e9a94e3a66536e
javiert01 <javotinoco@hotmail.com>
2020-04-19 18:31:11 -0500

    added wrong city popup

---
+++ src/app/dialogs/wrong-city/wrong-city.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-wrong-city',
+  templateUrl: './wrong-city.component.html',
+  styleUrls: ['./wrong-city.component.css']
+})
+export class WrongCityComponent implements OnInit {
+
+  constructor(private dialogRef: MatDialogRef<WrongCityComponent>) { }
+
+  ngOnInit(): void {
+  }
+
+  close(message) {
+    console.log('closing dialog');
+    this.dialogRef.close(message);
+  }
+
+}
