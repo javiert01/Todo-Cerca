95eb6f0163653d77c01e8fca6f6b66869c887e47
javiert01 <javotinoco@hotmail.com>
2020-04-30 13:57:27 -0500

    added firebase config

--- src/app/shared/var.constants.ts
+++ src/app/shared/var.constants.ts
@@ -2,8 +2,8 @@
 //export const HOST = "http://localhost:1337";
 
 /* BACKEND OPTIMUS DESARROLLO TEST
-export const HOST = "https://todosmascercapruebas.herokuapp.com";
+export const HOST = "https://todo-mas-cerca-1.herokuapp.com";
 */
 
-export const HOST = "https://todo-mas-cerca-1.herokuapp.com";
+export const HOST = "https://todosmascercapruebas.herokuapp.com";
 
