a2b5366c8459fd08d2433eecc4346f9c8b96d222
danielfryy <daniel.freire@trade.ec>
2020-06-14 11:51:08 -0500

    Add custom icon for current position and a basic permanent tooltip

--- src/app/components/map-os/map-os.component.ts
+++ src/app/components/map-os/map-os.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges, SimpleChange } from "@angular/core";
-import { Map, Marker, TileLayer, LeafletMouseEvent, LatLng } from "leaflet";
+import { Map, Marker, TileLayer, LeafletMouseEvent, LatLng, Icon, MarkerOptions, Tooltip } from "leaflet";
 import GestureHandling from "leaflet-gesture-handling";
 Map.addInitHook("addHandler", "gestureHandling", GestureHandling);
 
@@ -11,6 +11,7 @@ Map.addInitHook("addHandler", "gestureHandling", GestureHandling);
 export class MapOSComponent implements OnChanges, OnInit, AfterViewInit {
   @Input() center = new LatLng(-0.1840506, -78.503374);
   @Input() zoom = 18;
+  @Input() customIcon = false;
   @Output() mapClick = new EventEmitter<LatLng>();
   private _map: Map;
   private _marker: Marker;
@@ -40,7 +41,20 @@ export class MapOSComponent implements OnChanges, OnInit, AfterViewInit {
       maxZoom: 19,
     });
     tiles.addTo(this._map);
-    this._marker = new Marker(this.center);
+    const markerOptions: MarkerOptions = {};
+    if (this.customIcon) {
+      const icon = new Icon({
+        iconUrl: "assets/iconoverde-01.svg",
+        iconSize: [40, 50],
+        iconAnchor: [20, 50], // point of the icon which will correspond to marker's location
+      });
+      markerOptions.icon = icon;
+    }
+    this._marker = new Marker(this.center, markerOptions);
+    if (this.customIcon) {
+      const tooltip = new Tooltip({ permanent: true });
+      this._marker.bindTooltip(tooltip).setTooltipContent("Tú estás aquí").openTooltip();
+    }
     this._marker.addTo(this._map);
   }
 
